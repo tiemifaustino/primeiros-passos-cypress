07d1febb1d78612627a37c3ccd5de71220fb41dc
Tiemi Faustino <hayashipharma@gmail.com>
2025-07-16 11:29:50 -0300

    feat: :sparkles: Added userData for spec login

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -4,26 +4,39 @@ describe("Orange HRM Tests", () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    wrongCredentialAlert: "[role='alert']"
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: "[role='alert']",
+  };
+
+  const userData = {
+    userSuccess: {
+      username: "Admin",
+      password: "admin123",
+    },
+    userFail: {
+      username: "Test",
+      password: "test123",
+    },
   };
 
   it("Login - Success", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.get(selectorsList.usernameField).type("Admin");
-    cy.get(selectorsList.passwordField).type("admin123");
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username);
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password);
     cy.get(selectorsList.loginButton).click();
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
-    cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard");
+    // cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard");
+    cy.get(selectorsList.dashboardGrid);
   });
 
   it("Login - Fail", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.get(selectorsList.usernameField).type("Test");
-    cy.get(selectorsList.passwordField).type("test123");
+    cy.get(selectorsList.usernameField).type(userData.userFail.username);
+    cy.get(selectorsList.passwordField).type(userData.userFail.password);
     cy.get(selectorsList.loginButton).click();
     cy.get(selectorsList.wrongCredentialAlert);
   });
