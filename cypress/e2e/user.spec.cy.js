551b2e75505d188328ac19fc4ea453c6736c42f1
Tiemi Faustino <hayashipharma@gmail.com>
2025-07-17 16:58:32 -0300

    feat: :sparkles: Added genericDropdown and genericOption in selectorsList

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -16,8 +16,8 @@ describe("Orange HRM Tests", () => {
     dateCloseButton: ".--close",
     submitButton: "[type='submit']",
     toaster: ".oxd-toast-close",
-    dropdown: ".oxd-select-wrapper",
-    option: ".oxd-select-option"
+    genericDropdown: ".oxd-select-wrapper",
+    genericOption: ".oxd-select-option"
   };
 
   it("Login - Success", () => {
@@ -30,7 +30,7 @@ describe("Orange HRM Tests", () => {
     cy.get(selectorsList.myInfoButton).click();
     cy.get(selectorsList.firstNameField).clear().type("FirstNameTest");
     cy.get(selectorsList.lastName).clear().type("LastNameTest");
-    cy.get(selectorsList.genericField).eq(3).clear().type("Employee"); // para selecionar o elemento na posição 4 dp array
+    cy.get(selectorsList.genericField).eq(3).clear().type("Employee"); // para selecionar o elemento na posição 4 do array
     cy.get(selectorsList.genericField).eq(4).clear().type("OtherIdTest");
     cy.get(selectorsList.genericField)
       .eq(5)
@@ -38,10 +38,10 @@ describe("Orange HRM Tests", () => {
       .type("DriversLicenseNumberTest");
     cy.get(selectorsList.genericField).eq(6).clear().type("2025-03-10");
     cy.get(selectorsList.dateCloseButton).click();
-    cy.get(selectorsList.dropdown).eq(0).click()
-    cy.get(selectorsList.option).eq(129).click()
-    cy.get(selectorsList.dropdown).eq(1).click()
-    cy.get(selectorsList.option).eq(2).click()
+    cy.get(selectorsList.genericDropdown).eq(0).click()
+    cy.get(selectorsList.genericOption).eq(129).click()
+    cy.get(selectorsList.genericDropdown).eq(1).click()
+    cy.get(selectorsList.genericOption).eq(2).click()
     cy.get(selectorsList.genericField).eq(7).clear().type("2025-07-10");
     cy.get(selectorsList.dateCloseButton).click();
     cy.get(selectorsList.genericField).eq(8).clear().type("Test_FieldTest");
